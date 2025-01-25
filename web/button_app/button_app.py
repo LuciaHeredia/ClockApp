cf9b0f0f2de952e5e159bcff2bf9ea420f96a5c8
Lucia Heredia <luciaheredia22@gmail.com>
2025-01-25 16:01:44 +0200

    .py API request

--- web/button_app/button_app.py
+++ web/button_app/button_app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import os
 
 app = Flask(__name__)
+# api clock_app endpoint url
+clock_app_url = os.getenv('CLOCK_APP_URL')
 
 @app.route('/')
 def index():
     return render_template('button_index.html')
 
+@app.route('/subtract_minutes', methods=['POST'])
+def subtract_minutes():
+    # get minutes input from html file
+    minutes_to_sub = int(request.form['minutes']) 
+
+    # make API call to clock_app to subtract time
+    response = requests.post(f'{clock_app_url}/update_time', data={'minutes': minutes_to_sub})
+
+    # check response
+    if response.status_code == 200:
+        return 'Time subtracted!'
+    else:
+        return 'Failed to subtract time!'
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0", port=80)
--- web/clock_app/clock_app.py
+++ web/clock_app/clock_app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from datetime import datetime
+from flask import Flask, render_template, request, jsonify
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 current_time = datetime.now()
@@ -9,5 +9,16 @@ def index():
     return render_template('clock_index.html', 
                            current_time = current_time)
 
+@app.route('/update_time', methods=['POST'])
+def update_time():
+    # to modify a global variable inside a function.
+    global current_time 
+    # get minutes to subtract from button_app POST action
+    minutes_to_sub = int(request.form['minutes']) 
+    # update time in page: subtract minutes(time in general) between 2 datetime objects.
+    current_time -= timedelta(minutes=minutes_to_sub)
+    # return a JSON response with the content {"status": "success"} and a HTTP status code of 200, indicating a successful request.
+    return jsonify({'status': 'success'}), 200
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    app.run(debug=True, host="0.0.0.0", port=8000)
